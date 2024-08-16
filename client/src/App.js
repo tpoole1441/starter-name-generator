8a5ba2c293b32bc6da28f6275555077dc54130df
Tyler Poole <tyler.poole1441@gmail.com>
2024-08-16 09:54:48 -0500

    updated fetch url

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,15 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 const App = () => {
-  const [name, setName] = useState([])
+  const [name, setName] = useState([]);
 
   useEffect(() => {
-    fetch('https://enigmatic-shore-92552.herokuapp.com/generate')
+    fetch("https://starter-name-generator-8daw.onrender.com/generate")
       .then((res) => res.json())
-      .then((nme) => setName([nme]))
-  }, [])
+      .then((nme) => setName([nme]));
+  }, []);
 
-  return (
-    <h1>{name}</h1>
-  )
-}
+  return <h1>{name}</h1>;
+};
 
-export default App
+export default App;
