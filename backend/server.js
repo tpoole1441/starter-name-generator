8dcebcb0881836d200f3525852356bc8e0d57bf6
Tyler Poole <tyler.poole1441@gmail.com>
2024-08-16 09:51:30 -0500

    npm audit fix

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const router = require('./routes')
+const express = require("express");
+const router = require("./routes");
 
-const app = express()
-const PORT = process.env.PORT || 5000
+const app = express();
+const PORT = process.env.PORT || 5001;
 
-app.use('/', router)
+app.use("/", router);
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT} `)
-})
+  console.log(`Server running on ${PORT} `);
+});
 
-module.exports = app
+module.exports = app;
